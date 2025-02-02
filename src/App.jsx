661ed876e42f983d34a0bc31d3da511c3af4561e
firasHalouani <91092850+firasHalouani@users.noreply.github.com>
2025-02-02 23:36:51 +0100

    seconde commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductList from './components/productList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// eslint-disable-next-line no-unused-vars
+
+function App() {
+  return (
+    <div>
+      <ProductList/>
+    </div>
+  )
+}
+
+export default App
